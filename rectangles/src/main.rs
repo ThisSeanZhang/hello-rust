84bdacf03b7d0acb7c741d43e1c7911787483e48
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-24 16:44:38 +0800

    为Rectangle定义方法

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,14 +3,17 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    println!("rect1 is {:?}", rect1); // 加上了debug的注解才能进行格式化输出
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
- fn area(rectangle: &Rectangle) -> u32 {
-     rectangle.width * rectangle.height
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
