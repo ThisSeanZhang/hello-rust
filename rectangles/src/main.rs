098933fe728adc27f1791a9df0c221921cf36a22
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-24 11:35:26 +0800

    添加debug 注解

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,11 @@
+#[derive(Debug)] // 加上debug的注解
 struct Rectangle {
     width: u32,
     height: u32,
 }
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    println!("rect1 is {:?}", rect1); // 加上了debug的注解才能进行格式化输出
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
