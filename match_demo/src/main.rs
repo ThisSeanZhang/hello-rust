58d0809705edeef4c7ec4d3acff1c2b9297bd41e
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-25 08:52:15 +0800

    Update main.rs

--- match_demo/src/main.rs
+++ match_demo/src/main.rs
@@ -1,19 +1,26 @@
 fn main() {
     println!("Hello, world!");
-
+    value_in_cents(Coin::Quarter(UsState:: Alaska));
 }
-
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 fn value_in_cents(coin: Coin) -> u32 {
      match coin {
          Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter (state)=> {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
