6af06648c7e784ba95be6f93bf7d68577b61e783
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-25 09:02:28 +0800

    Update main.rs

--- match_demo/src/main.rs
+++ match_demo/src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
     println!("Hello, world!");
     value_in_cents(Coin::Quarter(UsState:: Alaska));
+
+    /**
+    * _用于匹配其他值(可以用具体的单词进行命名)
+    */
+    let some_u8_value = 4u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => println!("other: {}", _),
+    }
+
 }
 #[derive(Debug)]
 enum UsState {
