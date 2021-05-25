49d434a8cd97fff27429de13cc5f84c149db6e90
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-25 09:02:24 +0800

    update option

--- option_demo/src/main.rs
+++ option_demo/src/main.rs
@@ -9,4 +9,16 @@ fn main() {
     let some_string = Some("a string");
 
     let absent_number:Option<i32> = None;
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    // println!("six: {}, none: {}", six, none)
+
 }
