02c5af771cc296ec27c9b0a6bf2467a6a21f530a
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-25 08:56:17 +0800

    move to option demo

--- enum_demo/src/main.rs
+++ enum_demo/src/main.rs
@@ -58,14 +58,6 @@ fn main() {
     let m = Message::Write(String::from("hello"));
     m.call();
 
-    /**
-    * 常见且实用的Option
-    */
-
-    let some_number = Some(5);
-    let some_string = Some("a string");
-
-    let absent_number:Option<i32> = None;
 
 }
 
---
+++ option_demo/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+
+    /**
+    * 常见且实用的Option
+    */
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number:Option<i32> = None;
+}
