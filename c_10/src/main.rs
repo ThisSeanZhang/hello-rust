fc643f916193b0994964219b37240ecd17311bfc
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-27 15:46:24 +0800

    Update main.rs

--- c_10/src/main.rs
+++ c_10/src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     println!("The largest number is {}", largest(&number_list));
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    println!("The largest number is {}", largest(&number_list));
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result)
 }
 
 fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
