2ec469954e421ebac4a0c7fb76f55ab529b4b457
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-27 15:59:35 +0800

    Update main.rs

--- c_10/src/main.rs
+++ c_10/src/main.rs
@@ -4,7 +4,16 @@ fn main() {
 
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest_char(&char_list);
-    println!("The largest char is {}", result)
+    println!("The largest char is {}", result);
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    println!("p.x = {}", both_float.x());
+    println!("distance_from_origin = {}", both_float.distance_from_origin());
+    // 因为类型不匹配  所以不能用该方法
+    // println!("distance_from_origin = {}", both_integer.distance_from_origin());
+    // 以下因为x,y的类型不匹配  将会抛错
+    // let integer_and_float = Point { x: 5, y: 4.0 };
 }
 
 fn largest(list: &[i32]) -> i32 {
@@ -16,6 +25,7 @@ fn largest(list: &[i32]) -> i32 {
     }
     largest
 }
+
 fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
     for &item in list.iter() {
@@ -24,4 +34,21 @@ fn largest_char(list: &[char]) -> char {
         }
     }
     largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+// 也可为单独的类型设置方法
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
