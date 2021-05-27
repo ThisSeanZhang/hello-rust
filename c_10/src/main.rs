1fc36723fc79d59718e08ea4578f86489ae3212d
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-27 15:43:19 +0800

    Update main.rs

--- c_10/src/main.rs
+++ c_10/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
+    println!("The largest number is {}", largest(&number_list));
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    println!("The largest number is {}", largest(&number_list));
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
-    println!("The largest number is {}", largest);
+    largest
 }
