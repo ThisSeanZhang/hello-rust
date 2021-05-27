1406313eaf225e42def401a608f73a108b565c3f
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-27 23:01:52 +0800

    use trail override largest function

--- c_10/src/main.rs
+++ c_10/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     println!("The largest number is {}", largest(&number_list));
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 
     let both_integer = Point { x: 5, y: 10 };
@@ -16,16 +16,25 @@ fn main() {
     // let integer_and_float = Point { x: 5, y: 4.0 };
 }
 
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-    for &item in list.iter() {
+// fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list.iter() {
         if item > largest {
             largest = item;
         }
     }
     largest
 }
-
 fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
     for &item in list.iter() {
