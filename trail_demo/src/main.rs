e161bfc007870e6dab5af20b002bdc093630a26a
ThisSeanZhang <thisseanzhang@gmail.com>
2021-05-27 16:29:33 +0800

    trail demo

---
+++ trail_demo/src/main.rs
@@ -0,0 +1,16 @@
+mod trait_demo;
+
+use crate::trait_demo::*;
+
+fn main() {
+    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+
+}
+
