bcfacf81340d0e56afc0a38aa25b60598570da46
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-02 22:07:18 +0800

    create Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,14 +3,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &String::from("something");
-    let filename = &String::from("poem.txt");
+    let config = parse_config(&[String::from("target\\debug\\minigrep.exe"), String::from("something"), String::from("poem.txt")]);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
     println!("With text:\n{}", contents);
 
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {
+        query,
+        filename
+    }
 }
