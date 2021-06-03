c995a38679d0fa88c2babeac4b9d9e0179686c48
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-03 08:47:22 +0800

    在创建Config实例失败时使用错误码退出程序

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&[String::from("target\\debug\\minigrep.exe"), String::from("something"), String::from("poem.txt")]);
+    let args = [String::from("target\\debug\\minigrep.exe"), String::from("something"), String::from("poem.txt")];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
