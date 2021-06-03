15effa7f97c1a6218f403684021698493c6ea13d
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-03 16:59:25 +0800

    移动非main函数到lib文件中

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs, process};
 use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,36 +16,9 @@ fn main() {
     println!("In file {}", config.filename);
 
     // run(config);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)
-        .expect("something went wrong reading the file");
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {
-            query,
-            filename
-        })
-    }
-}
