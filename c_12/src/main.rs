5b6955add65d7e68e7aeadda2064ade0aebe2098
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-11 17:13:53 +0800

    使用Cacher来缓存结果

--- c_12/src/main.rs
+++ c_12/src/main.rs
@@ -10,6 +10,33 @@ fn main() {
     );
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T> where T: Fn(u32) -> u32{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
@@ -17,14 +44,30 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+
+    // 一般闭包会进行自动推导 可以不进行推导
+    // let expensive_closure = |num: u32| -> u32 {
+    // let expensive_closure = |num| {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
+
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps",
-            simulated_expensive_calculation(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -32,8 +75,17 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "today, run for {} minutes",
-                simulated_expensive_calculation(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
+
+}
+
+fn test() {
+    // 虽然闭包的类型可以进行推导，但是不能进行两次不同类型的调用
+    // 以下进行编译时会出现错误
+    // let example_closure = |x| x;
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
 }
