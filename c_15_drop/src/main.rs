84e5fb601bae370cb6675ba54fc0de9989d1e0bb
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-22 09:39:01 +0800

    提前调用drop进行释放变量

--- c_15_drop/src/main.rs
+++ c_15_drop/src/main.rs
@@ -10,4 +10,9 @@ fn main() {
     let c = CustomSmartPointer { data: String::from("my stuff") };
     let d = CustomSmartPointer { data: String::from("other stuff") };
     println!("CustomSmartPointers created.");
+
+    let c = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
