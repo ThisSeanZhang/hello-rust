21e235494261b5fa29ab51448ea3906bae6506e9
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-23 11:18:00 +0800

    trait

---
+++ c_17_2/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
