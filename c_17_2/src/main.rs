11b0926d7023639bf74ff861835e0f4638c04e7c
ThisSeanZhang <thisseanzhang@gmail.com>
2021-06-23 15:30:23 +0800

    Update main.rs

--- c_17_2/src/main.rs
+++ c_17_2/src/main.rs
@@ -1,5 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
 }
 
 pub struct Screen {
@@ -11,5 +30,48 @@ impl Screen {
         for component in self.components.iter() {
             component.draw();
         }
+    }
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+//
+// impl<T> Screen<T>
+//     where
+//         T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
     }
 }
