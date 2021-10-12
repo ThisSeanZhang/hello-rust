0a01996ccd2d0c255a97b07b5cb1b9a04a7f986a
ThisSeanZhang <thisseanzhang@gmail.com>
2021-10-12 23:40:24 +0800

    v3

--- c_20_1/src/lib.rs
+++ c_20_1/src/lib.rs
@@ -1,7 +1,22 @@
 use std::thread;
 pub struct ThreadPool {
-  threads: Vec<thread::JoinHandle<()>>,
+  workers: Vec<Worker>,
 }
+
+struct Worker {
+  id: usize,
+  handle: thread::JoinHandle<()>,
+}
+
+impl Worker {
+  fn new(id: usize) -> Worker {
+    Worker{
+      id,
+      handle: thread::spawn(|| {})
+    }
+  }
+}
+
 impl ThreadPool {
   /// 创建线程池
   /// 
@@ -11,10 +26,14 @@ impl ThreadPool {
   /// 当size小于等于0时抛出错误
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
-    let threads = Vec::with_capacity(size);
+    let mut workers = Vec::with_capacity(size);
     
+    for id in 0..size {
+      workers.push(Worker::new(id));
+    }
+
     ThreadPool {
-      threads
+      workers
     }
   }
 
