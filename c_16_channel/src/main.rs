ac0976a5b843263aa05a23f5c2dba04408893c8b
ThisSeanZhang <thisseanzhang@gmail.com>
2021-10-12 22:36:53 +0800

    Update main.rs

--- c_16_channel/src/main.rs
+++ c_16_channel/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         let val = String::from("hi");
         tx.send(val).unwrap();
         // 发送到通道后尝试使用将会抛错  因为所有权已经被转移
-        println!("val is {}", val);
+        // println!("val is {}", val);
     });
 
     let received = rx.recv().unwrap();
