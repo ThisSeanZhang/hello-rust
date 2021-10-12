702bcec25a2f7dc92b051770d3fca13e56d6a662
ThisSeanZhang <thisseanzhang@gmail.com>
2021-10-12 22:37:02 +0800

    v1

---
+++ c_20_1/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use std::net::{ TcpListener, TcpStream };
+use std::io::prelude::*;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!\n");
+        handle_connection(stream);
+        println!("\n");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    // println!("{}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+        let contents = fs::read_to_string(filename).unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+       
+}
+    
