5308f572b31ce558a4d11c70f4f1b605bbea8362
ThisSeanZhang <thisseanzhang@gmail.com>
2021-10-12 22:44:45 +0800

    add sleep case

--- c_20_1/src/main.rs
+++ c_20_1/src/main.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::net::{ TcpListener, TcpStream };
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection established!\n");
@@ -17,11 +19,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     // println!("{}", String::from_utf8_lossy(&buffer[..]));
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-        } else {
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-        };
+    };
         let contents = fs::read_to_string(filename).unwrap();
         let response = format!("{}{}", status_line, contents);
         stream.write(response.as_bytes()).unwrap();
