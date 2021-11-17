7b738d1c16baedd4c9a5db5de69695f20294b7e6
ThisSeanZhang <thisseanzhang@gmail.com>
2021-11-17 21:48:34 +0800

    u

--- c_08_vec/src/main.rs
+++ c_08_vec/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("Hello, world!");
     let v:Vec<i32> = Vec::new();
-
+    println!("{:?}", v);
     let v = vec![1, 2, 3];
+    println!("{:?}", v);
 
     let mut v = Vec::new();
     v.push(5);
@@ -42,6 +42,7 @@ fn main() {
 
     // 在动态数组中使用定义的枚举来存储不同类型的值
     // 如果无法列举出所有可能的情况话,还可以使用trait进行定义
+    #[derive(Debug)]
     enum SpreadsheetCell {
         Int(i32),
         Float(f64),
@@ -52,14 +53,19 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+    println!("{:?}", row);
 
     // String也是一种字符串
-    let mut s = String::new();
+    let s = String::new();
+    println!("{}", s);
     let data = "initial contents";
     let s = data.to_string();
+    println!("{}", s);
     // 这个方法同样也可以直接作用于字面量：
     let s = "initial contents".to_string();
+    println!("{}", s);
     let s = String::from("initial contents");
+    println!("{}", s);
 
     let mut s = String::from("foo");
     s.push_str("bar");
@@ -77,13 +83,13 @@ fn main() {
     let s2 = String::from("world!");
     let s3 = s1 + &s2; // 注意这里的s1已经被移动且再也不能被使用了
     // println!("s1: {}", s1);
-    /**
-    * 由于+ 的函数签名是fn add(self, s: &str) -> String
-    * self不是取引用,所以s1的所有权被转移,而s2使用的是&String传入了引用
-    * 将会被强转成&str 变为&s2[..]
-    * 即便let s3 = s1 + &s2;看起来像是复制两个字符串并创建一个新的字符串，但实际上这
-    * 条语句会取得s1的所有权，再将s2中的内容复制到其中，最后再将s1的所有权作为结果返回
-    **/
+    println!("s3: {}", s3);
+
+    // 由于+ 的函数签名是fn add(self, s: &str) -> String
+    // self不是取引用,所以s1的所有权被转移,而s2使用的是&String传入了引用
+    // 将会被强转成&str 变为&s2[..]
+    // 即便let s3 = s1 + &s2;看起来像是复制两个字符串并创建一个新的字符串，但实际上这
+    // 条语句会取得s1的所有权，再将s2中的内容复制到其中，最后再将s1的所有权作为结果返回
 
     // 如果遇到需要拼接多个字符串的
 
@@ -91,11 +97,13 @@ fn main() {
     let s2 = String::from("tac");
     let s3 = String::from("toe");
     let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("{}", s);
 
     let s1 = String::from("tic");
     let s2 = String::from("tac");
     let s3 = String::from("toe");
     let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{}", s);
     // format 不会转移所有权
 
     // Rust的字符串不支持索引
@@ -104,8 +112,10 @@ fn main() {
 
     // 因为String的内部是使用u8进行 且使用utf-8进行编码
     let len = String::from("Hola").len();// 会返回4
+    println!("{}", len);
     // 但下方的长度就不是12了 实际是24 因为每个字符需要占用2个字节
     let len = String::from("Здравствуйте").len();
+    println!("{}", len);
     println!("&[0..2]: {}", &"Здравствуйте"[0..2]);
     // 以下的方式进行获取会报错,因为不是取到一个完整的字符, 所以不建议使用切片的方式获取字符
     // thread 'main' panicked at 'byte index 3 is not a char boundary; it is inside 'д' (bytes 2..4) of `Здравствуйте`', src\main.rs:110:30
@@ -116,11 +126,11 @@ fn main() {
 
     // 遍历字符
     for c in "संस्कृतम्".chars() {
-        // println!("{}", c);
+        println!("{}", c);
     }
     // 合法的Unicode标量值可能会需要占用1字节以上的空间
     for b in "संस्कृतम्".bytes() {
-        // println!("{}", b);
+        println!("{}", b);
     }
 
 }
