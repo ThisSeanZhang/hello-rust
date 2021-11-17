e3b5b5251cd0901770a6487a3a043b9dc670dd79
ThisSeanZhang <thisseanzhang@gmail.com>
2021-11-17 20:40:45 +0800

    u

--- c_06_match/src/main.rs
+++ c_06_match/src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Dime);
     value_in_cents(Coin::Quarter(UsState:: Alaska));
+    value_in_cents(Coin::Quarter(UsState:: Alabama));
 
-    /**
-    * _用于匹配其他值(可以用具体的单词进行命名)
-    */
+    // 用于匹配其他值(可以用具体的单词进行命名)
     let some_u8_value = 4u8;
     match some_u8_value {
         1 => println!("one"),
         3 => println!("three"),
         5 => println!("five"),
         7 => println!("seven"),
-        _ => println!("other: {}", _),
+        _ => (), // 使用_匹配未能匹配的情况
     }
 
 }
@@ -28,10 +30,10 @@ enum Coin {
 }
 fn value_in_cents(coin: Coin) -> u32 {
      match coin {
-         Coin::Penny => 1,
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter (state)=> {
+        Coin::Quarter (state) => {
             println!("State quarter from {:?}!", state);
             25
         },
