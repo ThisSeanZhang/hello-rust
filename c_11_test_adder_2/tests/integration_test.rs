115dbc1b67839262080b36859d876b1c3d425b29
ThisSeanZhang <thisseanzhang@gmail.com>
2021-11-18 22:38:33 +0800

    u

--- c_11_test_adder_2/tests/integration_test.rs
+++ c_11_test_adder_2/tests/integration_test.rs
@@ -1,9 +1,9 @@
-use test_adder_2;
+use c_11_test_adder_2;
 // cargo test --test integration_test
 // 仅想测试一个test文件时 传入的文件名
 mod common2;
 #[test]
 fn it_adds_two() {
     common2::setup();
-    assert_eq!(4, test_adder_2::add_two(2));
+    assert_eq!(4, c_11_test_adder_2::add_two(2));
 }
