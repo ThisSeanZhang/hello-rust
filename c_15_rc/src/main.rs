b5ccd5d57df94589284451e8abe7bdfbbe0b2516
ThisSeanZhang <thisseanzhang@gmail.com>
2021-11-19 13:18:01 +0800

    Update main.rs

--- c_15_rc/src/main.rs
+++ c_15_rc/src/main.rs
@@ -8,7 +8,7 @@ use std::rc::Rc;
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    let c = Cons(4, a.clone());
 
 
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
