53532acf8f5a1f3dea3ae395f0f9a664c24ed4c2
ThisSeanZhang <thisseanzhang@gmail.com>
2022-01-04 00:07:05 +0800

    Update main.rs

--- programming-rust/c_03_basic_types/src/main.rs
+++ programming-rust/c_03_basic_types/src/main.rs
@@ -30,4 +30,93 @@ fn main() {
     assert_eq!(5f32.sqrt() * 5f32.sqrt(), 5.); // exactly 5.0, per IEEE
     assert_eq!(-1.01f64.floor(), -1.0);
     assert!((-1. / std::f32::INFINITY).is_sign_negative());
+
+    println!("{}", (2.0_f64).sqrt());
+    println!("{}", f64::sqrt(2.0));
+
+    assert_eq!(false as i32, 0);
+    assert_eq!(true as i32, 1);
+
+
+    assert_eq!('*' as i32, 42);
+    assert_eq!('*' as u16, 0x2A);
+    // assert_eq!('*' as i8, -(i8::MAX - 42)); // U+0CA0 truncated to eight bits, signed
+
+
+    let text = "I see the eigenvalue in thine eye";
+    let (head, tail) = text.split_at(21);
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+    
+    let temp = text.split_at(21);
+    let head = temp.0;
+    let tail = temp.1;
+    assert_eq!(head, "I see the eigenvalue ");
+    assert_eq!(tail, "in thine eye");
+
+    // Arrays
+    let lazy_caterer: [u32; 6] = [1, 2, 4, 7, 11, 16];
+    let taxonomy = ["Animalia", "Arthropoda", "Insecta"];
+    assert_eq!(lazy_caterer[3], 7);
+    assert_eq!(taxonomy.len(), 3);
+
+    let mut sieve = [true; 10000];
+    for i in 2..100 {
+        if sieve[i] {
+            let mut j = i * i;
+            while j < 10000 {
+                sieve[j] = false;
+                j += i;
+            }
+        }
+    }
+    assert!(sieve[211]);
+    assert!(!sieve[9876]);
+
+    let mut chaos = [3, 5, 4, 1, 2];
+    chaos.sort();
+    assert_eq!(chaos, [1, 2, 3, 4, 5]);
+
+    // Vectors
+    let mut v = vec![2, 3, 5, 7];
+    assert_eq!(v.iter().fold(1, |a, b| a * b), 210);
+
+    v.push(11);
+    v.push(13);
+    assert_eq!(v.iter().fold(1, |a, b| a * b), 30030);
+
+    fn new_pixel_buffer(rows: usize, cols: usize) -> Vec<u8> {
+        vec![0; rows * cols]
+    }
+
+    let test = new_pixel_buffer(1,1);
+    println!("{:?}", test);
+
+    let mut v = Vec::new();
+    v.push("step");
+    v.push("on");
+    v.push("no");
+    v.push("pets");
+    assert_eq!(v, vec!["step", "on", "no", "pets"]);
+
+    let v: Vec<i32> = (0..5).collect();
+    assert_eq!(v, [0, 1, 2, 3, 4]);
+
+    // A palindrome!
+    let mut v = vec!["a man", "a plan", "a canal", "panama"];
+    v.reverse();
+    // Reasonable yet disappointing:
+    assert_eq!(v, vec!["panama", "a canal", "a plan", "a man"]);
+
+
+    let mut v = Vec::with_capacity(2);
+    assert_eq!(v.len(), 0);
+    assert_eq!(v.capacity(), 2);
+    v.push(1);
+    v.push(2);
+    assert_eq!(v.len(), 2);
+    assert_eq!(v.capacity(), 2);
+    v.push(3);
+    assert_eq!(v.len(), 3);
+    assert_eq!(v.capacity(), 4);
 }
