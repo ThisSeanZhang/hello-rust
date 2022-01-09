7a43d778b46f7fa065159a2e8034f1dfa811164b
ThisSeanZhang <thisseanzhang@gmail.com>
2022-01-09 20:44:36 +0800

    Update main.rs

--- programming-rust/c_04_ownership/src/main.rs
+++ programming-rust/c_04_ownership/src/main.rs
@@ -24,4 +24,54 @@ fn main() {
     for composer in &composers {
         println!("{}, born {}", composer.name, composer.birth);
     }
+
+    // let x = vec![10, 20, 30];
+    // if c {
+    //     f(x); // ... ok to move from x here
+    // } else {
+    //     g(x); // ... and ok to also move from x here
+    // }
+    // h(x) // bad: x is uninitialized here if either path uses it
+
+    // let x = vec![10, 20, 30];
+    // while f() {
+    //     g(x); // bad: x would be moved in first iteration,
+    // // uninitialized in second
+    // }
+
+    // let mut x = vec![10, 20, 30];
+    // while f() {
+    //     g(x); // move from x
+    //     x = h(); // give x a fresh value
+    // }
+    // e(x);
+    
+
+    // Build a vector of the strings "101", "102", ... "105"
+    let mut v = Vec::new();
+    for i in 101 .. 106 {
+        v.push(i.to_string());
+    }
+    // Pull out random elements from the vector.
+    // let third = v[2];
+    // let fifth = v[4];
+
+
+    // Build a vector of the strings "101", "102", ... "105"
+    let mut v = Vec::new();
+    for i in 101 .. 106 {
+    v.push(i.to_string());
+    }
+    // 1. Pop a value off the end of the vector:
+    let fifth = v.pop().unwrap();
+    assert_eq!(fifth, "105");
+    // 2. Move a value out of the middle of the vector, and move the last
+    // element into its spot:
+    let second = v.swap_remove(1);
+    assert_eq!(second, "102");
+    // 3. Swap in another value for the one we're taking out:
+    let third = std::mem::replace(&mut v[2], "substitute".to_string());
+    assert_eq!(third, "103");
+    // Let's see what's left of our vector.
+    assert_eq!(v, vec!["101", "104", "substitute"]);
 }
