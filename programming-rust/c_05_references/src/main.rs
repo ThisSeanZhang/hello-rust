45e51ee364ff7b772cb1fbad36be2f85ea5c1324
ThisSeanZhang <thisseanzhang@gmail.com>
2022-01-17 01:32:00 +0800

    Update main.rs

--- programming-rust/c_05_references/src/main.rs
+++ programming-rust/c_05_references/src/main.rs
@@ -7,11 +7,12 @@ fn main() {
     table.insert("Gesualdo".to_string(), vec!["many madrigals".to_string(), "Tenebrae Responsoria".to_string()]);
     table.insert("Caravaggio".to_string(), vec!["The Musicians".to_string(), "The Calling of St. Matthew".to_string()]);
     table.insert("Cellini".to_string(), vec!["Perseus with the head of Medusa".to_string(), "a salt cellar".to_string()]);
-    show(table);
+    show(&table);
+    sort_works(&mut table);
 }
 
 
-fn show(table: Table) {
+fn show(table: &Table) {
     for (artist, works) in table {
         println!("works by {}:", artist);
         for work in works {
@@ -19,4 +20,33 @@ fn show(table: Table) {
         }
     }
 }
-   
+
+fn sort_works(table: &mut Table) {
+    for (_artist, works) in table {
+        works.sort();
+    }
+}
+
+#[test]
+fn references_as_value() {
+    struct Anime { name: &'static str, bechdel_pass: bool };
+    let aria = Anime { name: "Aria: The Animation", bechdel_pass: true };
+    let anime_ref = &aria;
+    assert_eq!(anime_ref.name, "Aria: The Animation");
+    // Equivalent to the above, but with the dereference written out:
+    assert_eq!((*anime_ref).name, "Aria: The Animation");
+
+    let mut v = vec![1973, 1968];
+    v.sort(); // implicitly borrows a mutable reference to v
+    (&mut v).sort(); // equivalent; much uglier
+}
+
+#[test]
+fn assigning_references() {
+    let x = 10;
+    let y = 20;
+    let mut r = &x;
+    let b= true;
+    if b { r = &y; }
+    assert!(*r == 10 || *r == 20);
+}
