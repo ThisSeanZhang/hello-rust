59a2be5f0dc2fec0d795f52272f6c1b05be2f1cd
ThisSeanZhang <thisseanzhang@gmail.com>
2022-02-07 11:07:52 +0800

    Update main.rs

--- programming-rust/c_05_references/src/main.rs
+++ programming-rust/c_05_references/src/main.rs
@@ -1,3 +1,4 @@
+use core::borrow;
 use std::collections::HashMap;
 
 type Table = HashMap<String, Vec<String>>;
@@ -49,4 +50,141 @@ fn assigning_references() {
     let b= true;
     if b { r = &y; }
     assert!(*r == 10 || *r == 20);
+}
+
+#[test]
+fn references_references() {
+    struct Point { x: i32, y: i32 }
+    let point = Point { x: 1000, y: 729 };
+    let r: &Point = &point;
+    let rr: &&Point = &r;
+    let rrr: &&&Point = &rr;
+
+    assert_eq!(rrr.y, 729);
+}
+
+#[test]
+fn comparing_references() {
+    let x = 10;
+    let y = 10;
+    let rx = &x;
+    let ry = &y;
+    let rrx = &rx;
+    let rry = &ry;
+    println!("{}", rx);
+    println!("{}", rrx);
+    assert!(rrx <= rry);
+    assert!(rrx == rry);
+
+    assert!(!std::ptr::eq(rx, ry));
+}
+
+#[test]
+fn borrowing_references_arbitrary() {
+    fn factorial(n: usize) -> usize {
+        (1..n+1).fold(1, |a, b| a * b)
+    }
+    let r = &factorial(6);
+    assert_eq!(r + &1009, 1729);
+       
+}
+
+#[test]
+fn borrowing_local_variable() {
+    // {
+    //     let r;
+    //     {
+    //     let x = 1;
+    //     r = &x;
+    //     }
+    //     assert_eq!(*r, 1); // bad: reads memory `x` used to occupy
+    // }
+}
+
+#[test]
+fn returning_references() {
+    // v should have at least one element.
+    fn smallest(v: &[i32]) -> &i32 {
+        let mut s = &v[0];
+        for r in &v[1..] {
+        if *r < *s { s = r; }
+        }
+        s
+    }
+
+    let s;
+    {
+     let parabola = [9, 4, 1, 0, 1, 4, 9];
+     s = smallest(&parabola);
+    }
+    // assert_eq!(*s, 0); // bad: points to element of dropped array
+}
+
+#[test]
+fn structs_containing_referen() {
+    // This does not compile.
+// struct S {
+//     r: &i32
+//    }
+//    let s;
+//    {
+//     let x = 10;
+//     s = S { r: &x };
+//    }
+//    assert_eq!(*s.r, 10); // bad: reads from dropped `x`
+   
+}
+
+#[test]
+fn test_borrowing_part() {
+    // let mut w = (107, 109);
+    // let r = &w;
+    // let r0 = &r.0; // ok: reborrowing shared as shared
+    // let m1 = &mut r.1; // error: can't reborrow shared as mutable
+}
+
+#[test]
+fn test_borrowing_part2() {
+    let mut w = (10, 42);
+    let r0 = &mut w.0;
+    let r1 = &w.1;
+
+    println!("{}, {}", r0, r1);
+}
+
+#[test]
+fn test_string_asref() {
+    fn as_test<A: AsRef<[u8]>>(data: A) {
+        println!("{:?}", data.as_ref());
+    }
+    as_test("aaa".to_string());
+}
+
+#[test]
+fn borrow_hashmap() {
+    struct Foo {
+        name: String
+    }
+
+    impl std::borrow::Borrow<str> for Foo {
+        fn borrow(&self) -> &str {
+            &self.name
+        }
+    }
+
+    fn need_str<A>(data: A)
+    where A: std::borrow::Borrow<str> {
+        println!("{}", data.borrow());
+    }
+    need_str(Foo{name: "cccc".to_string()})
+}
+
+#[test]
+fn test_string() {
+    String::from("");
+    let mut a = "a";
+    let b: String = a.to_owned();
+    a = "b";
+    println!("{}", a);
+    println!("{}", b);
 }
