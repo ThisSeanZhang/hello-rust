5f2790470238904c36fc85b5798d2b9917b3f230
ThisSeanZhang <thisseanzhang@gmail.com>
2022-06-22 00:21:37 +0800

    Update main.rs

--- CS61B-SP21/lectureCode/lec4_list1/src/main.rs
+++ CS61B-SP21/lectureCode/lec4_list1/src/main.rs
@@ -1,9 +1,14 @@
+use std::time::Instant;
+
 mod int_list;
 mod s_list;
 
 fn main() {
     let mut slist = s_list::SList::new(None);
+    let now = Instant::now();
     for i in 0..10_000_000 {
         slist.add_first(i);
     }
+    let elapsed_time = now.elapsed();
+    println!("use ing timeing: {}", elapsed_time.as_millis());
 }
