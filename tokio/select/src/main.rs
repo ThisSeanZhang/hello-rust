46cf6de93246d57fe7d849916b0838de519c8ce3
ThisSeanZhang <thisseanzhang@gmail.com>
2023-03-26 22:14:55 +0800

    bing 的解法

--- tokio/select/src/main.rs
+++ tokio/select/src/main.rs
@@ -1,8 +1,8 @@
 
 use std::time::{Instant, Duration};
 
+use futures::FutureExt;
 use tokio::select;
-
 // 定义一个异步函数，它会延迟3秒后返回"Hello"
 async fn say_hello() -> &'static str {
     tokio::time::sleep(std::time::Duration::from_secs(3)).await;
@@ -12,12 +12,11 @@ async fn say_hello() -> &'static str {
 // 定义一个异步函数，它会使用select!宏等待say_hello或者5秒后触发
 async fn wait_or_timeout() {
     // 创建一个延迟5秒的Sleep Future，并调用fuse方法
-    let timeout = tokio::time::sleep(tokio::time::Duration::from_secs(5));
-    tokio::pin!(timeout);
+    let mut timeout = Box::pin(tokio::time::sleep(std::time::Duration::from_secs(5)).fuse());
     // 创建一个say_hello Future，并调用fuse方法
-    let mut hello = Box::pin(say_hello());
+    let mut hello = say_hello().fuse();
     // 使用pin_mut宏将hello Future固定在栈上
-    
+    futures::pin_mut!(hello);
     let time = Instant::now();
     // 使用select!宏同时等待timeout和hello
     loop {
@@ -26,14 +25,13 @@ async fn wait_or_timeout() {
         () = timeout.as_mut() => {
             println!("Timeout");
             // 重置timeout Future
-            timeout.as_mut().reset(tokio::time::Instant::now() + std::time::Duration::from_secs(5));
+            timeout.set(tokio::time::sleep(std::time::Duration::from_secs(5)).fuse());
         }
         // 如果hello完成了，打印它的返回值
         result = hello.as_mut() => {
             println!("Result: {}", result);
             // 重置hello Future
-            
-            hello.set(say_hello());
+            hello.set(say_hello().fuse());
             if time.elapsed() > Duration::from_secs(20) {
                 break;
             }
