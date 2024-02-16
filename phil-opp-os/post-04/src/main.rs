324bbe8ee66af9db4f57ff7f6e14f715549a8293
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-16 09:27:42 -0500

    post-04-harness-test

--- phil-opp-os/post-04/src/main.rs
+++ phil-opp-os/post-04/src/main.rs
@@ -4,17 +4,21 @@
 // 开启自定义的测试方法 需要在 no_std 的情况下使用
 #![feature(custom_test_frameworks)]
 // 所有 test 的处理函数
-#![test_runner(crate::test_runner)]
+#![test_runner(post_04::test_runner)]
 // test 的入口函数被重命名了
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
-// #[cfg(not(test))]
 use core::panic::PanicInfo;
+use post_04::println;
 
-/// This function is called on panic.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+    loop {}
+}
 
 /// https://github.com/rust-lang/rust-analyzer/issues/4490
 #[cfg(not(test))]
@@ -27,29 +31,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
-    test_main();
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    // 退出 QEMU 的状态
-    exit_qemu(QemuExitCode::Success);
+    post_04::test_panic_handler(info)
 }
 
 #[test_case]
@@ -57,33 +39,3 @@ fn trivial_assertion() {
     assert_eq!(1, 1);
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
--- phil-opp-os/post-04/tests/basic_boot.rs
+++ phil-opp-os/post-04/tests/basic_boot.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(post_04::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use post_04::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -13,11 +14,13 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    post_04::test_panic_handler(info)
 }
