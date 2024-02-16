77b9521e12f14f9d3e9fb3f7bcf007ae1949a756
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-16 08:52:11 -0500

    post-04-create-lib

--- phil-opp-os/post-04/src/main.rs
+++ phil-opp-os/post-04/src/main.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
 
+// 开启自定义的测试方法 需要在 no_std 的情况下使用
 #![feature(custom_test_frameworks)]
+// 所有 test 的处理函数
 #![test_runner(crate::test_runner)]
-
+// test 的入口函数被重命名了
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
@@ -41,21 +43,18 @@ pub extern "C" fn _start() {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
-    println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     // 退出 QEMU 的状态
-    // exit_qemu(QemuExitCode::Success);
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
