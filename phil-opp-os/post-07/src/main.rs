b8f4ca7e34b634e941b0230f057008d47bc67b84
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-21 20:46:01 -0500

    post-07-v1

---
+++ phil-opp-os/post-07/src/main.rs
@@ -0,0 +1,53 @@
+#![no_std]
+#![no_main]
+
+// 开启自定义的测试方法 需要在 no_std 的情况下使用
+#![feature(custom_test_frameworks)]
+// 所有 test 的处理函数
+#![test_runner(post_07::test_runner)]
+// test 的入口函数被重命名了
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use post_07::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    post_07::init();
+
+    // fn stack_overflow() {
+    //     stack_overflow(); // for each recursion, the return address is pushed
+    // }
+
+    // // trigger a stack overflow
+    // stack_overflow();
+    
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    loop {}
+}
+
+/// https://github.com/rust-lang/rust-analyzer/issues/4490
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    post_07::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
