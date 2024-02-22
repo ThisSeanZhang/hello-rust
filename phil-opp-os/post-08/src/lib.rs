f82f7893228943bbec68b496392a9e75cf00d703
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-22 02:59:36 -0500

    post-08

---
+++ phil-opp-os/post-08/src/lib.rs
@@ -0,0 +1,95 @@
+#![no_std]
+
+#![cfg_attr(test, no_main)]
+
+
+// 开启自定义的测试方法 需要在 no_std 的情况下使用
+#![feature(custom_test_frameworks)]
+// 所有 test 的处理函数
+#![test_runner(crate::test_runner)]
+// test 的入口函数被重命名了
+#![reexport_test_harness_main = "test_main"]
+
+// 开启 x86 abi
+#![feature(abi_x86_interrupt)] 
+
+pub mod vga_buffer;
+pub mod serial;
+pub mod interrupts;
+pub mod gdt;
+
+use core::panic::PanicInfo;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    hlt_loop();
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    init();
+    test_main();
+    hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable(); 
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
