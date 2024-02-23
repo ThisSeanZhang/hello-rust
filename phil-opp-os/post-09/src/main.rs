68d1a73a32dfcb1dd0f6a738b6be639cec5ba3ea
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-23 04:06:17 -0500

    post-09-v3

--- phil-opp-os/post-09/src/main.rs
+++ phil-opp-os/post-09/src/main.rs
@@ -18,14 +18,14 @@ entry_point!(kernel_main);
 #[no_mangle]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // use post_09::memory::active_level_4_table;
-    use post_09::memory::translate_addr;
-    use x86_64::VirtAddr;
+    use post_09::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
     // use x86_64::structures::paging::PageTable;
     
     println!("Hello World{}", "!");
     post_09::init();
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-
+    let mapper = unsafe { memory::init(phys_mem_offset) };
     let addresses = [
         // the identity-mapped vga buffer page
         0xb8000,
@@ -36,33 +36,14 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         // virtual address mapped to physical address 0
         boot_info.physical_memory_offset,
     ];
-    
+
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        // 使用 mapper 进行翻译内存地址
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
-    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-
-    // for (i, entry) in l4_table.iter().enumerate() {
-    //     if !entry.is_unused() {
-    //         println!("L4 Entry {}: {:?}", i, entry);
-        
-    //         // get the physical address from the entry and convert it
-    //         let phys = entry.frame().unwrap().start_address();
-    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
-    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
-    //         let l3_table: &PageTable = unsafe { &*ptr };
-        
-    //         // print non-empty entries of the level 3 table
-    //         for (i, entry) in l3_table.iter().enumerate() {
-    //             if !entry.is_unused() {
-    //                 println!("  L3 Entry {}: {:?}", i, entry);
-    //             }
-    //         }
-    //     }
-    // }
+    
     // as before
     #[cfg(test)]
     test_main();
--- phil-opp-os/post-09/src/memory.rs
+++ phil-opp-os/post-09/src/memory.rs
@@ -3,6 +3,18 @@ use x86_64::{
     VirtAddr,
 };
 use x86_64::PhysAddr;
+use x86_64::structures::paging::OffsetPageTable;
+
+/// Initialize a new OffsetPageTable.
+///
+/// This function is unsafe because the caller must guarantee that the
+/// complete physical memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`. Also, this function must be only called once
+/// to avoid aliasing `&mut` references (which is undefined behavior).
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
 
 /// Returns a mutable reference to the active level 4 table.
 ///
@@ -10,7 +22,7 @@ use x86_64::PhysAddr;
 /// complete physical memory is mapped to virtual memory at the passed
 /// `physical_memory_offset`. Also, this function must be only called once
 /// to avoid aliasing `&mut` references (which is undefined behavior).
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
 -> &'static mut PageTable
 {
     use x86_64::registers::control::Cr3;
