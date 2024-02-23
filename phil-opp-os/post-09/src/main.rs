fc6acbd1167a2050175b1014831fdf923217cb84
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-23 02:06:43 -0500

    post-09-v1

---
+++ phil-opp-os/post-09/src/main.rs
@@ -0,0 +1,51 @@
+#![no_std]
+#![no_main]
+
+// 开启自定义的测试方法 需要在 no_std 的情况下使用
+#![feature(custom_test_frameworks)]
+// 所有 test 的处理函数
+#![test_runner(post_09::test_runner)]
+// test 的入口函数被重命名了
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use post_09::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    post_09::init();
+
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    post_09::hlt_loop();
+}
+
+/// https://github.com/rust-lang/rust-analyzer/issues/4490
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    post_09::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    post_09::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
