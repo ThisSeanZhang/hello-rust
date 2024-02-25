192580073f619cc95ff711342f68a40f9a26c3e4
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-25 04:24:34 -0500

    post-10

---
+++ phil-opp-os/post-10/src/main.rs
@@ -0,0 +1,74 @@
+#![no_std]
+#![no_main]
+
+// 开启自定义的测试方法 需要在 no_std 的情况下使用
+#![feature(custom_test_frameworks)]
+// 所有 test 的处理函数
+#![test_runner(post_10::test_runner)]
+// test 的入口函数被重命名了
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
+
+use post_10::println;
+
+entry_point!(kernel_main);
+
+#[no_mangle]
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    // use post_10::memory::active_level_4_table;
+    use post_10::memory;
+    use post_10::memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::Page, VirtAddr}; 
+    // use x86_64::structures::paging::PageTable;
+    
+    println!("Hello World{}", "!");
+    post_10::init();
+
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    // let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    // 要求分配不存在的内存时将会发生崩溃, 创建了内存分配器 就不会错误了
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    // map an unused page
+    // let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
+    post_10::hlt_loop();
+}
+
+/// https://github.com/rust-lang/rust-analyzer/issues/4490
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    post_10::hlt_loop();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    post_10::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+
