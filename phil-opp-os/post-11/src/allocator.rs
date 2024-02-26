b87b6491bd6219c33fa10431e87529f33cd4f66f
ThisSeanZhang <thisseanzhang@gmail.com>
2024-02-26 01:42:50 -0500

    post-11-bump

--- phil-opp-os/post-11/src/allocator.rs
+++ phil-opp-os/post-11/src/allocator.rs
@@ -8,10 +8,17 @@ use x86_64::{
     VirtAddr,
 };
 
-use linked_list_allocator::LockedHeap;
+
+pub mod bump;
+
+use bump::BumpAllocator;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+// use linked_list_allocator::LockedHeap;
+// #[global_allocator]
+// static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 // #[global_allocator]
 // static ALLOCATOR: Dummy = Dummy;
@@ -58,6 +65,30 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
 
 
