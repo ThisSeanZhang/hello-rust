36ab65dc6de2941cba81eecce8b7a8bff5525e78
ThisSeanZhang <thisseanzhang@gmail.com>
2024-04-01 15:59:49 +0800

    update embed 02

--- embedded/discovery-microbit/02-led-roulette/src/main.rs
+++ embedded/discovery-microbit/02-led-roulette/src/main.rs
@@ -3,28 +3,52 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use microbit::board::Board;
-use microbit::hal::prelude::*;
-use microbit::hal::timer::Timer;
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
+
+const PIXELS: [(usize, usize); 16] = [
+    (0, 0),
+    (0, 1),
+    (0, 2),
+    (0, 3),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (3, 4),
+    (4, 4),
+    (4, 3),
+    (4, 2),
+    (4, 1),
+    (4, 0),
+    (3, 0),
+    (2, 0),
+    (1, 0),
+];
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
 
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
 
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let mut last_led = (0, 0);
 
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Dark!");
-        timer.delay_ms(1_000_u16);
-        row1.set_high().unwrap();
-        rprintln!("Light!");
-        timer.delay_ms(1_000_u16);
+        for current_led in PIXELS.iter() {
+            leds[last_led.0][last_led.1] = 0;
+            leds[current_led.0][current_led.1] = 1;
+            display.show(&mut timer, leds, 30);
+            last_led = *current_led;
+        }
     }
 }
