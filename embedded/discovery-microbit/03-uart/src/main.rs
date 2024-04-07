43584baa39fc0ae691f67c0ace794f2a1aa92fac
ThisSeanZhang <thisseanzhang@gmail.com>
2024-04-07 14:25:29 +0800

    embed 03 -2

--- embedded/discovery-microbit/03-uart/src/main.rs
+++ embedded/discovery-microbit/03-uart/src/main.rs
@@ -3,6 +3,7 @@
 use core::fmt::Write;
 
 use cortex_m_rt::entry;
+use heapless::Vec;
 use microbit::{
     hal::prelude::*,
     hal::uarte,
@@ -29,9 +30,31 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).unwrap();
-    write!(serial, "The quick brown fox jumps over the lazy dog.\r\n").unwrap();
-    nb::block!(serial.flush()).unwrap();
+    // nb::block!(serial.write(b'X')).unwrap();
+    // write!(serial, "The quick brown fox jumps over the lazy dog.\r\n").unwrap();
+    // nb::block!(serial.flush()).unwrap();
 
-    loop {}
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+    loop {
+        buffer.clear();
+
+        loop {
+            // We assume that the receiving cannot fail
+            let byte = nb::block!(serial.read()).unwrap();
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "error: buffer full\r\n").unwrap();
+                break;
+            }
+
+            if byte == 13 {
+                for byte in buffer.iter().rev().chain(&[b'\n', b'\r']) {
+                    nb::block!(serial.write(*byte)).unwrap();
+                }
+                break;
+            }
+        }
+        nb::block!(serial.flush()).unwrap()
+    }
 }
