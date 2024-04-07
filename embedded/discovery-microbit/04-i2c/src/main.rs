8fbc3069572a7477c74b3d773cdbfecb85fe9ed0
ThisSeanZhang <thisseanzhang@gmail.com>
2024-04-08 01:00:01 +0800

    i2c-3

--- embedded/discovery-microbit/04-i2c/src/main.rs
+++ embedded/discovery-microbit/04-i2c/src/main.rs
@@ -1,37 +1,87 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+use core::str;
+
 use cortex_m_rt::entry;
+use heapless::Vec;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
 use microbit::hal::prelude::*;
-use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
+use microbit::{
+    hal::twim,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+    pac::twim0::frequency::FREQUENCY_A,
+};
+use nb::block;
 use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
+use serial_setup::UartePort;
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
-
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+mod serial_setup;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
-    let mut acc = [0];
-    let mut mag = [0];
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc)
-        .unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag)
-        .unwrap();
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
 
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
+    loop {
+        let mut buffer: Vec<u8, 32> = Vec::new();
 
-    loop {}
+        loop {
+            let byte = block!(serial.read()).unwrap();
+
+            if byte == 13 {
+                break;
+            }
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "error: buffer full\r\n").unwrap();
+                break;
+            }
+        }
+
+        if str::from_utf8(&buffer).unwrap().trim() == "accelerometer" {
+            while !sensor.accel_status().unwrap().xyz_new_data {}
+
+            let data = sensor.accel_data().unwrap();
+            write!(
+                serial,
+                "Accelerometer: x {} y {} z {}\r\n",
+                data.x, data.y, data.z
+            )
+            .unwrap();
+        } else if str::from_utf8(&buffer).unwrap().trim() == "magnetometer" {
+            while !sensor.mag_status().unwrap().xyz_new_data {}
+
+            let data = sensor.mag_data().unwrap();
+            write!(
+                serial,
+                "Magnetometer: x {} y {} z {}\r\n",
+                data.x, data.y, data.z
+            )
+            .unwrap();
+        } else {
+            write!(serial, "error: command not detected\r\n").unwrap();
+        }
+    }
 }
