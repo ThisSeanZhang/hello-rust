e28e1d045385f61d4dbe404bd28e1aaa287b3a84
ThisSeanZhang <thisseanzhang@gmail.com>
2024-04-09 21:14:40 +0800

    compass-magnitude

--- embedded/discovery-microbit/05-led-compass/src/main.rs
+++ embedded/discovery-microbit/05-led-compass/src/main.rs
@@ -7,19 +7,13 @@ use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
 
 mod calibration;
-use crate::calibration::calc_calibration;
-use crate::calibration::calibrated_measurement;
-
-mod led;
-use core::f32::consts::PI;
-
-use libm::atan2f;
+use libm::sqrtf;
 use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
 use microbit::{display::blocking::Display, hal::Timer};
 use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
 
-use crate::led::direction_to_led;
-use crate::led::Direction;
+use crate::calibration::calc_calibration;
+use crate::calibration::calibrated_measurement;
 
 #[entry]
 fn main() -> ! {
@@ -44,31 +38,10 @@ fn main() -> ! {
         while !sensor.mag_status().unwrap().xyz_new_data {}
         let mut data = sensor.mag_data().unwrap();
         data = calibrated_measurement(data, &calibration);
-
-        // use libm's atan2f since this isn't in core yet
-        let theta = atan2f(data.y as f32, data.x as f32);
-
-        // Figure out the direction based on theta
-        let dir = if theta < -7. * PI / 8. {
-            Direction::West
-        } else if theta < -5. * PI / 8. {
-            Direction::SouthWest
-        } else if theta < -3. * PI / 8. {
-            Direction::South
-        } else if theta < -PI / 8. {
-            Direction::SouthEast
-        } else if theta < PI / 8. {
-            Direction::East
-        } else if theta < 3. * PI / 8. {
-            Direction::NorthEast
-        } else if theta < 5. * PI / 8. {
-            Direction::North
-        } else if theta < 7. * PI / 8. {
-            Direction::NorthWest
-        } else {
-            Direction::West
-        };
-
-        display.show(&mut timer, direction_to_led(dir), 100);
+        let x = data.x as f32;
+        let y = data.y as f32;
+        let z = data.z as f32;
+        let magnitude = sqrtf(x * x + y * y + z * z);
+        rprintln!("{} nT, {} mG", magnitude, magnitude / 100.0);
     }
 }
