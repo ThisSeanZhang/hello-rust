c70fc78c13b58b2e85f73241d7a2c914eece4992
ThisSeanZhang <thisseanzhang@gmail.com>
2024-04-09 21:12:08 +0800

    compass-task-2

--- embedded/discovery-microbit/05-led-compass/src/main.rs
+++ embedded/discovery-microbit/05-led-compass/src/main.rs
@@ -11,6 +11,9 @@ use crate::calibration::calc_calibration;
 use crate::calibration::calibrated_measurement;
 
 mod led;
+use core::f32::consts::PI;
+
+use libm::atan2f;
 use lsm303agr::{AccelOutputDataRate, Lsm303agr, MagOutputDataRate};
 use microbit::{display::blocking::Display, hal::Timer};
 use microbit::{hal::twim, pac::twim0::frequency::FREQUENCY_A};
@@ -42,20 +45,30 @@ fn main() -> ! {
         let mut data = sensor.mag_data().unwrap();
         data = calibrated_measurement(data, &calibration);
 
-        let dir = match (data.x > 0, data.y > 0) {
-            // Quadrant I
-            (true, true) => Direction::NorthEast,
-            // Quadrant II
-            (false, true) => Direction::NorthWest,
-            // Quadrant III
-            (false, false) => Direction::SouthWest,
-            // Quadrant IV
-            (true, false) => Direction::SouthEast,
+        // use libm's atan2f since this isn't in core yet
+        let theta = atan2f(data.y as f32, data.x as f32);
+
+        // Figure out the direction based on theta
+        let dir = if theta < -7. * PI / 8. {
+            Direction::West
+        } else if theta < -5. * PI / 8. {
+            Direction::SouthWest
+        } else if theta < -3. * PI / 8. {
+            Direction::South
+        } else if theta < -PI / 8. {
+            Direction::SouthEast
+        } else if theta < PI / 8. {
+            Direction::East
+        } else if theta < 3. * PI / 8. {
+            Direction::NorthEast
+        } else if theta < 5. * PI / 8. {
+            Direction::North
+        } else if theta < 7. * PI / 8. {
+            Direction::NorthWest
+        } else {
+            Direction::West
         };
 
-        // use the led module to turn the direction into an LED arrow
-        // and the led display functions from chapter 5 to display the
-        // arrow
         display.show(&mut timer, direction_to_led(dir), 100);
     }
 }
