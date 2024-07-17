a2eb87fc450192ef66e2656f87e743449a04772f
ThisSeanZhang <thisseanzhang@gmail.com>
2024-07-17 07:57:21 +0000

    ADD: uefi init

---
+++ hello-uefi/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# cargo build
+# cp target/x86_64-unknown-uefi/debug/hello-uefi.efi esp/efi/boot/bootx64.efi
+qemu-system-x86_64 -enable-kvm \
+    -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
+    -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
+    -drive format=raw,file=fat:rw:esp
