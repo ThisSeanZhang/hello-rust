98808dfce1e9d101a575d2014246c35c2a9b054d
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 11:34:49 +0000

    trait-api: v8, using type driven

--- example/trait-example/src/main.rs
+++ example/trait-example/src/main.rs
@@ -2,58 +2,85 @@ use std::{collections::HashSet, thread::sleep, time::Duration};
 
 const CLEAR : &str = "\x1B[2J\x1B[1;1H";
 
-struct Progress<Iter> {
-    iter:Iter,
-    i: usize,
-    bound: Option<usize>,
+struct Unbounded;
+struct Bounded {
+    bound: usize,
     delims: (char, char)
 }
 
-impl <Iter> Progress<Iter> {
-    pub fn new(iter:Iter) -> Self {
-        Progress { iter, i: 0, bound: None, delims: ('[', ']') }
+struct Progress<Iter, Bound> {
+    iter:Iter,
+    i: usize,
+    bound: Bound
+}
+
+trait ProgressDisplay: Sized {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>);
+}
+
+impl ProgressDisplay for Unbounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        println!("{}", "*".repeat(progress.i))
     }
 }
 
-impl <Iter> Progress<Iter>
+impl ProgressDisplay for Bounded {
+    fn display<Iter>(&self, progress: &Progress<Iter, Self>) {
+        
+        println!("{}{}{}{}", self.delims.0, "*".repeat(progress.i), " ".repeat(self.bound - progress.i), self.delims.1)
+    }
+}
+
+impl <Iter> Progress<Iter, Unbounded> {
+    pub fn new(iter:Iter) -> Self {
+        Progress { iter, i: 0, bound: Unbounded }
+    }
+}
+
+impl <Iter> Progress<Iter, Unbounded>
 where Iter: ExactSizeIterator {
 
-    pub fn with_bound(mut self) -> Self{
-        self.bound = Some(self.iter.len());
-        self
+    pub fn with_bound(mut self) -> Progress<Iter, Bounded>{
+        let bound = Bounded {
+            bound: self.iter.len(),
+            delims: ('[', ']')
+        };
+        Progress {
+            i: self.i,
+            iter: self.iter,
+            bound
+           
+        }
     }
 }
 
-impl <Iter> Progress<Iter> {
+impl <Iter> Progress<Iter, Bounded> {
     pub fn with_delims(mut self, delims: (char, char)) -> Self{
-        self.delims = delims;
+        self.bound.delims = delims;
         self
     }
 }
 
-impl <Iter> Iterator for Progress<Iter>
-where Iter: Iterator{
+impl <Iter, Bound> Iterator for Progress<Iter, Bound>
+where Iter: Iterator, Bound: ProgressDisplay {
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
         println!("{}", CLEAR);
-        match self.bound {
-            Some(bound) => println!("{}{}{}{}", self.delims.0, "*".repeat(self.i), " ".repeat(bound - self.i), self.delims.1),
-            None => println!("{}", "*".repeat(self.i))
-        }
+        self.bound.display(&self);
         self.i += 1;
         self.iter.next()
     }
 }
 
 trait ProgressIteratorExt: Sized {
-    fn progress(self) -> Progress<Self>;
+    fn progress(self) -> Progress<Self, Unbounded>;
 }
 
 /// for **all type** <Iter> impl ProgressIteratorExt
 impl<Iter> ProgressIteratorExt for Iter
 where Iter: Iterator {
-    fn progress(self) -> Progress<Self> {
+    fn progress(self) -> Progress<Self, Unbounded> {
         Progress::new(self)
     }
 }
@@ -64,14 +91,14 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
     let brkts = ('<', '>');
-    // with_delims not works, but not hit
-    // key is: Progress is stateful, and with_delims can be call  all status
-    for n in (0 ..).progress().with_delims(brkts) {
-        expensive_calculation(&n);
-    }
+    // now, the hit show: no method named `with_delims` found for struct
+    // for n in (0 ..).progress().with_delims(brkts) {
+    //     expensive_calculation(&n);
+    // }
 
     let v = vec![1, 2, 3];
 
+    // example: hit user with_bound must call before with_delims
     for n in v.iter().progress().with_bound().with_delims(brkts) {
         expensive_calculation(n);
     }
