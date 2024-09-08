330c925c78eefd1990f3227089e3b4e9379baad8
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 08:17:53 +0000

    trait-api: v3, using Iterator trait

--- example/trait-example/src/main.rs
+++ example/trait-example/src/main.rs
@@ -1,15 +1,16 @@
-use std::{thread::sleep, time::Duration};
+use std::{collections::HashSet, thread::sleep, time::Duration};
 
 const CLEAR : &str = "\x1B[2J\x1B[1;1H";
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 
-fn progress<T>(v: Vec<T>, f: fn(&T)->()) {
+fn progress<T, Iter>(iter: Iter, f: fn(T)->())
+where Iter: Iterator<Item = T>{
 
     let mut i = 1;
 
-    for n in v.iter() {
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
         f(n);
@@ -18,5 +19,10 @@ fn progress<T>(v: Vec<T>, f: fn(&T)->()) {
 
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v, expensive_calculation);
+    progress(v.iter(), expensive_calculation);
+
+    let mut h = HashSet::new();
+    h.insert(0);
+    progress(h.iter(), expensive_calculation);
+
 }
