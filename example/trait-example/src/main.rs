ee70edfd30e384b1d2131bfc362896f4efa9d9bc
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 09:33:33 +0000

    trait-api: v4, using struct replace function

--- example/trait-example/src/main.rs
+++ example/trait-example/src/main.rs
@@ -1,28 +1,39 @@
 use std::{collections::HashSet, thread::sleep, time::Duration};
 
 const CLEAR : &str = "\x1B[2J\x1B[1;1H";
+
+struct Progress<Iter> {
+    iter:Iter,
+    i: usize,
+}
+
+impl <Iter> Progress<Iter> {
+    pub fn new(iter:Iter) -> Self {
+        Progress { iter, i: 0 }
+    }
+}
+
+impl <Iter> Iterator for Progress<Iter>
+where Iter: Iterator{
+    type Item = Iter::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        
+        println!("{}{}", CLEAR, "*".repeat(self.i));
+        self.i += 1;
+        self.iter.next()
+    }
+}
+
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 
-fn progress<Iter>(iter: Iter, f: fn(Iter::Item)->())
-where Iter: Iterator{
-
-    let mut i = 1;
-
-    for n in iter {
-        println!("{}{}", CLEAR, "*".repeat(i));
-        i += 1;
-        f(n);
-    }
-}
-
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v.iter(), expensive_calculation);
 
-    let mut h = HashSet::new();
-    h.insert(0);
-    progress(h.iter(), expensive_calculation);
+    for n in Progress::new(v.iter()) {
+        expensive_calculation(n);
+    }
 
 }
