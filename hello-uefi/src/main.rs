ec632c1a9768835edc84b36c5174f9d5185b3a6d
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 14:44:27 +0800

    UPDATE: UEFI MAIN

--- hello-uefi/src/main.rs
+++ hello-uefi/src/main.rs
@@ -3,11 +3,49 @@
 
 use log::info;
 use uefi::prelude::*;
+use uefi::proto::device_path::text::{
+    AllowShortcuts, DevicePathToText, DisplayOnly,
+};
+use uefi::proto::loaded_image::LoadedImage;
+use uefi::table::boot::SearchType;
+use uefi::{Identify, Result};
 
 #[entry]
-fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi::helpers::init(&mut system_table).unwrap();
-    info!("Hello world!");
-    system_table.boot_services().stall(10_000_000);
+    let boot_services = system_table.boot_services();
+
+    print_image_path(boot_services).unwrap();
+
+    boot_services.stall(10_000_000);
     Status::SUCCESS
+}
+
+fn print_image_path(boot_services: &BootServices) -> Result {
+    let loaded_image = boot_services
+        .open_protocol_exclusive::<LoadedImage>(boot_services.image_handle())?;
+
+    let device_path_to_text_handle = *boot_services
+        .locate_handle_buffer(SearchType::ByProtocol(&DevicePathToText::GUID))?
+        .first()
+        .expect("DevicePathToText is missing");
+
+    let device_path_to_text = boot_services
+        .open_protocol_exclusive::<DevicePathToText>(
+            device_path_to_text_handle,
+        )?;
+
+    let image_device_path =
+        loaded_image.file_path().expect("File path is not set");
+    let image_device_path_text = device_path_to_text
+        .convert_device_path_to_text(
+            boot_services,
+            image_device_path,
+            DisplayOnly(true),
+            AllowShortcuts(false),
+        )
+        .expect("convert_device_path_to_text failed");
+
+    info!("Image path: {}", &*image_device_path_text);
+    Ok(())
 }
