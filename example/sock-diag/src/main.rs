10999d5976854c2a5249562f9f28f48239419e9a
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 06:47:04 +0000

    sock_diag  example

---
+++ example/sock-diag/src/main.rs
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+use netlink_packet_core::{
+    NetlinkHeader, NetlinkMessage, NetlinkPayload, NLM_F_DUMP, NLM_F_REQUEST,
+};
+use netlink_packet_sock_diag::{
+    constants::*,
+    inet::{ExtensionFlags, InetRequest, SocketId, StateFlags},
+    SockDiagMessage,
+};
+use netlink_sys::{protocols::NETLINK_SOCK_DIAG, Socket, SocketAddr};
+
+fn main() {
+    let mut socket = Socket::new(NETLINK_SOCK_DIAG).unwrap();
+    let _port_number = socket.bind_auto().unwrap().port_number();
+    socket.connect(&SocketAddr::new(0, 0)).unwrap();
+
+    let mut nl_hdr = NetlinkHeader::default();
+    nl_hdr.flags = NLM_F_REQUEST | NLM_F_DUMP;
+    let mut packet = NetlinkMessage::new(
+        nl_hdr,
+        SockDiagMessage::InetRequest(InetRequest {
+            family: AF_INET,
+            protocol: IPPROTO_TCP,
+            extensions: ExtensionFlags::empty(),
+            states: StateFlags::all(),
+            socket_id: SocketId::new_v4(),
+        })
+        .into(),
+    );
+
+    packet.finalize();
+
+    let mut buf = vec![0; packet.header.length as usize];
+
+    // Before calling serialize, it is important to check that the buffer in
+    // which we're emitting is big enough for the packet, other
+    // `serialize()` panics.
+    assert_eq!(buf.len(), packet.buffer_len());
+
+    packet.serialize(&mut buf[..]);
+
+    println!(">>> {packet:?}");
+    if let Err(e) = socket.send(&buf[..], 0) {
+        println!("SEND ERROR {e}");
+        return;
+    }
+
+    let mut receive_buffer = vec![0; 4096];
+    let mut offset = 0;
+    while let Ok(size) = socket.recv(&mut &mut receive_buffer[..], 0) {
+        loop {
+            let bytes = &receive_buffer[offset..];
+            let rx_packet =
+                <NetlinkMessage<SockDiagMessage>>::deserialize(bytes).unwrap();
+            println!("<<< {rx_packet:?}");
+
+            match rx_packet.payload {
+                NetlinkPayload::Noop | NetlinkPayload::Error(_) => {}
+                NetlinkPayload::InnerMessage(
+                    SockDiagMessage::InetResponse(response),
+                ) => {
+                    println!("{response:#?}");
+                }
+                NetlinkPayload::Done(_) => {
+                    println!("Done!");
+                    return;
+                }
+                _ => return,
+            }
+
+            offset += rx_packet.header.length as usize;
+            if offset == size || rx_packet.header.length == 0 {
+                offset = 0;
+                break;
+            }
+        }
+    }
+}
