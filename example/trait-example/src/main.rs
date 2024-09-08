bdb8d0aa04ea0d220a5283246f618f2161ba31e3
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 08:03:11 +0000

    trait-api: concrete function

--- example/trait-example/src/main.rs
+++ example/trait-example/src/main.rs
@@ -1,5 +1,6 @@
 use std::{thread::sleep, time::Duration};
 
+const CLEAR : &str = "\x1B[2J\x1B[1;1H";
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
@@ -7,7 +8,11 @@ fn expensive_calculation(_n: &i32) {
 fn main() {
     let v = vec![1, 2, 3];
 
+    let mut i = 1;
+
     for n in v.iter() {
+        println!("{}{}", CLEAR, "*".repeat(i));
+        i += 1;
         expensive_calculation(n);
     }
 }
