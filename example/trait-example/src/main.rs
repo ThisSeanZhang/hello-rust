6b37a0f981eff989a119be0e89a04ce5d23df190
ThisSeanZhang <thisseanzhang@gmail.com>
2024-09-08 10:58:58 +0000

    trait-api: v6, using ExactSizeIterator

--- example/trait-example/src/main.rs
+++ example/trait-example/src/main.rs
@@ -5,11 +5,21 @@ const CLEAR : &str = "\x1B[2J\x1B[1;1H";
 struct Progress<Iter> {
     iter:Iter,
     i: usize,
+    bound: Option<usize>
 }
 
 impl <Iter> Progress<Iter> {
     pub fn new(iter:Iter) -> Self {
-        Progress { iter, i: 0 }
+        Progress { iter, i: 0, bound: None }
+    }
+}
+
+impl <Iter> Progress<Iter>
+where Iter: ExactSizeIterator {
+
+    pub fn with_bound(mut self) -> Self{
+        self.bound = Some(self.iter.len());
+        self
     }
 }
 
@@ -18,8 +28,11 @@ where Iter: Iterator{
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        
-        println!("{}{}", CLEAR, "*".repeat(self.i));
+        println!("{}", CLEAR);
+        match self.bound {
+            Some(bound) => println!("[{}{}]", "*".repeat(self.i), " ".repeat(bound - self.i)),
+            None => println!("{}", "*".repeat(self.i))
+        }
         self.i += 1;
         self.iter.next()
     }
@@ -43,9 +56,14 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
 
+    // The method can be used, but a compilation error will be prompted: "ExactSizeIterator  was not satisfied"
+    // for n in (0 ..).progress().with_bound() {
+    //     expensive_calculation(&n);
+    // }
+
     let v = vec![1, 2, 3];
 
-    for n in v.iter().progress() {
+    for n in v.iter().progress().with_bound() {
         expensive_calculation(n);
     }
 
